07f4f66504107a0cb2abf99a7f041a00ccd2a8c2
fscozzatti <fscozzatti@gmail.com>
2021-05-20 15:51:37 -0300

    Creación e integración del componente AppFrame y refactorización de MainPage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import Paper from '@material-ui/core/Paper'
+import AppFrame from './../components/AppFrame'
+
+
 
 const MainPage = () => {
+            
     return (
-        <div>
-            Pagina encontrada
-        </div>
+        <AppFrame>
+            <Paper elevation={3}>
+                HOLA MUNDO!!!
+            </Paper>
+        </AppFrame>
     )
 }
+
 
 export default MainPage
