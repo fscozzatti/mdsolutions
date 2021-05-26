e6dc43214799b8fba0c731bf5c12fe29a3a22160
fscozzatti <fscozzatti@gmail.com>
2021-05-26 15:05:01 -0300

    Se le dió el formato requerido a la página R3

--- src/pages/R3.jsx
+++ src/pages/R3.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Paper from '@material-ui/core/Paper'
-import AppFrame from './../components/AppFrame'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import ReactPlayer from 'react-player'
+import AppFrame from './../components/AppFrame'
 
 
 
@@ -11,40 +12,46 @@ const R3 = () => {
     return (
         <AppFrame>
             <Paper elevation={3}>
-                <Grid container
-                    direction="row"
-                    justify="center"
-                    alignItems="center" 
-                    spacing={3}
-                >
-                    <Grid item
-                    xs={12}
-                    sm={6}                    
+                <div className= "container-fluid py-3">
+                    <Grid container 
+                        spacing={3}
                     >
-
-                    </Grid>
-                    <Grid item xs={12}
-                     sm container
-                     direction="column"
-                     spacing={3}
-                     >
                         <Grid item
                         xs={12}
                         sm={6}                    
                         >
-                            <Typography variant="h4" color="black">
-                                Lección de vida importante
-                            </Typography>                          
+                            <ReactPlayer url='https://www.youtube.com/watch?v=PABq5NEMhuU&ab_channel=RODOLFOROBLEDORODOLFOROBLEDO'
+                            width='100%'
+                            height='100%'
+                            controls
+                            muted
+                            />
                         </Grid>
-                        <Grid item
-                        xs={12}
-                        sm={6}                     
+                        <Grid item xs={12}
+                        sm container
+                        direction="column"
+                        justify="flex-start"
+                        alignItems="strech"
                         >
-                            <p variant="h1" color="black">Fragmento tomado de una película en la cual Will Smith
-                            le dice a su hijo un consejo muy importante, el cual queda retratado en este maravilloso video</p>                        
+                            <div className="container-fluid py-5">
+                                <Grid item
+                                xs={12}
+                                sm={6}                    
+                                >
+                                    <Typography variant="h4">
+                                        Lección de vida importante
+                                    </Typography>                          
+                                </Grid>
+                                <Grid item
+                                xs={12}
+                                sm={6}                     
+                                >
+                                    <p variant="h1" color="black">Fragmento tomado de una película en la cual Will Smith le dice a su hijo un consejo muy importante, el cual queda retratado en este maravilloso video</p>                        
+                                </Grid>
+                            </div>
                         </Grid>
                     </Grid>
-                </Grid>
+                </div>
             </Paper>
         </AppFrame>
     )
